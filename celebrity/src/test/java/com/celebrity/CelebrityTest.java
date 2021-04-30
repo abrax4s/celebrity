97c2e718c9f49a98191174b044a22bc428824830
abrax4s <adn.mtzjmz@gmail.com>
2021-04-30 16:04:53 -0500

    Add files via upload

--- celebrity/src/test/java/com/celebrity/CelebrityTest.java
+++ celebrity/src/test/java/com/celebrity/CelebrityTest.java
@@ -3,22 +3,49 @@ package com.celebrity;
 import static org.junit.Assert.*;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
+import org.junit.After;
 import org.junit.Test;
 
 public class CelebrityTest {
 
+	@After
+	public void notifyTest() {
+		System.out.println("----Test finished-----");
+	}
 	@Test
 	public void testFindCelebrity() {
-		boolean[][] acquaitances = { { false, false, true, false }, { false, false, true, false },
-				{ false, false, false, false }, { false, false, true, false } };
+		System.out.println("Test with celebrity");
 		List<Person> people = new ArrayList<>();
-		people.add(new Person(0));
-		people.add(new Person(1));
-		people.add(new Person(2));
+		people.add(new Person(1,Arrays.asList(new Person(1), new Person(2), new Person(3), new Person(4))));
+		people.add(new Person(2,Arrays.asList(new Person(1), new Person(2), new Person(3), new Person(4))));
 		people.add(new Person(3));
-		new Celebrity().findCelebrity(people, acquaitances);
+		people.add(new Person(4,Arrays.asList(new Person(1), new Person(2), new Person(3), new Person(4))));
+		new Celebrity().findCelebrity(people);
+	}
+	
+	@Test
+	public void testFindNoCelebrity() {
+		System.out.println("Test with no celebrities");
+		List<Person> people = new ArrayList<>();
+		people.add(new Person(1,Arrays.asList(new Person(1), new Person(2), new Person(3), new Person(4))));
+		people.add(new Person(2,Arrays.asList(new Person(1), new Person(2), new Person(3), new Person(4))));
+		people.add(new Person(3,Arrays.asList(new Person(1), new Person(2), new Person(3), new Person(4))));
+		people.add(new Person(4,Arrays.asList(new Person(1), new Person(2), new Person(3), new Person(4))));
+		new Celebrity().findCelebrity(people);
+	}
+	
+	@Test
+	public void testFindCelebrities() {
+		System.out.println("Test with more than one celebrity");
+		List<Person> people = new ArrayList<>();
+		people.add(new Person(1,Arrays.asList(new Person(1), new Person(2), new Person(3), new Person(4))));
+		people.add(new Person(2));
+		people.add(new Person(3,Arrays.asList(new Person(1), new Person(2), new Person(3), new Person(4))));
+		people.add(new Person(4));
+		new Celebrity().findCelebrity(people);
 	}
 
 }
