83cf6a03248f42ef8bb3b4925a02a6d36b7f1759
abrax4s <adn.mtzjmz@gmail.com>
2021-04-30 16:04:11 -0500

    Add files via upload

--- celebrity/src/main/java/com/celebrity/Celebrity.java
+++ celebrity/src/main/java/com/celebrity/Celebrity.java
@@ -1,34 +1,29 @@
 package com.celebrity;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Celebrity {
 
 	/**
 	 * Find celebrity method.
 	 * 
-	 * Runs two iteration over people list to compare each position on the list to
-	 * the rest of the collection, if all list has been compared and the current
-	 * position knows no people on the list, returns that object, otherwise the
-	 * second loop continues. If no celebrity is discovered, returns null.
-	 * 
-	 * @param people list of Person
-	 * @return Person who will be known as celebrity.
+	 * Iterates over provided list of people (Person objects) to find people who
+	 * don't know anyone in the group. Those persons are considered celebrities.
 	 */
-	
-	public void findCelebrity(List<Person> people, boolean[][] acquaitances) {                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                       
-		people.stream().forEach(personA ->{
-			int counter= 0;
-			for(Person personB : people) {
-				if(!personA.knowsPerson(personB, acquaitances)) {
-					counter ++;
-					if(counter==people.size()) {
-						System.out.println("Celebrity is: "+personA.getId());
-					}
-				}else {
-					continue;
-				}
-			}
-		});
+
+	public void findCelebrity(List<Person> people) {
+		List<Person> celebrities = people.stream()
+				.filter(personA -> (null == personA.getKnownPeople() || 0 == personA.getKnownPeople().size()))
+				.collect(Collectors.toList());
+
+		if (celebrities.size() == 1) {
+			System.out.println("Celebrity is: " + celebrities.get(0).getId());
+		} else if (celebrities.size() > 1) {
+			System.out.println("Celebrities are:");
+			celebrities.stream().map(celebrity -> celebrity.getId()).forEach(System.out::println);
+		} else {
+			System.out.println("No celebrity in the list");
+		}
 	}
 }
--- celebrity/src/main/java/com/celebrity/Person.java
+++ celebrity/src/main/java/com/celebrity/Person.java
@@ -1,23 +1,34 @@
 package com.celebrity;
 
+import java.util.List;
+
 public class Person {
-	private int id;
-	
-	public Boolean knowsPerson(Person person,boolean[][] acquaitances) {
-		Boolean res = null;
-		res = acquaitances[this.id][person.id];
-		return res;
-	}
+	private Integer id;
+	private List<Person> knownPeople;
+
 	public int getId() {
 		return id;
 	}
-	public void setId(int id) {
+
+	public void setId(Integer id) {
 		this.id = id;
 	}
 
-	public Person(int id) {
+	public List<Person> getKnownPeople() {
+		return knownPeople;
+	}
+
+	public void setKnownPeople(List<Person> knownPeople) {
+		this.knownPeople = knownPeople;
+	}
+
+	public Person(Integer id, List<Person> knownPeople) {
 		super();
 		this.id = id;
+		this.knownPeople = knownPeople;
 	}
-	
+
+	public Person(Integer id) {
+		this.id = id;
+	}
 }
